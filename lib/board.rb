87d46b0bded735c1641406dee86e89c438df5d88
Alastair Lloyd <dex8056@aol.com>
2022-10-20 10:52:06 -0600

    test: Add and pass place method

--- lib/board.rb
+++ lib/board.rb
@@ -67,4 +67,13 @@ class Board
       false
     end
   end
+
+  def place(ship, coordinates)
+    if valid_placement?(ship, coordinates)
+      coordinates.each do |coordinate|
+      @game_board[coordinate].place_ship(ship)
+      end
+    end
+  end
 end
+
