f484c7279e56e2d68d791c207834d3f7223244b3
Alastair Lloyd <dex8056@aol.com>
2022-10-20 10:50:25 -0600

    fix: Stop cells creating a new board when board exists

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,28 @@
 class Board
+  attr_reader :game_board
 
-  def cells
-    coordinates_array = ["A1","A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
-    game_board = Hash.new
-    coordinates_array.each do |coordinate|
-      game_board[coordinate] = Cell.new(coordinate)
-    end
-    game_board
+  def initialize
+    @game_board
   end
 
-  def valid_coordinate?(coordinate)
-    if cells.key?(coordinate)
-      true
+  def cells
+    if game_board == nil 
+      coordinates_array = ["A1","A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+      game_board = Hash.new
+      coordinates_array.each do |coordinate|
+        game_board[coordinate] = Cell.new(coordinate)
+      end
+      @game_board = game_board
     else
-      false
+      @game_board
+    end
+  end
+
+    def valid_coordinate?(coordinate)
+     if cells.key?(coordinate)
+       true
+     else
+       false
     end
   end
 
