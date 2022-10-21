a782df6bfb14faace97898aaa1b3354e6dd241de
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-21 17:01:11 -0500

    feat: Add computer ship placement method for game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -3,8 +3,10 @@ require './lib/cell'
 require './lib/board'
 
 class Game
-
-  def initialize #may need may not......
+  attr_reader :computer_board
+  def initialize
+    @computer_board = Board.new
+     #may need may not......
   end
 
   def player1 #thought it would be helpful to have a method to call on when user inputs rather than keep writing same code.
@@ -17,7 +19,8 @@ class Game
     input = player1
 
     if input == "p"
-      #run game
+      run_game
+      puts @computer_board.render(true)
     elsif input == "q"
       puts "Thanks for playing" #goodbye statement or similar
     else
@@ -25,10 +28,26 @@ class Game
     end
   end
 
+  def run_game
+    @computer_board.cells
+    computer_ship_placement
+  end
 
-
-
-
+  def computer_ship_placement
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    coordinates_array = ["A1","A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    coordinates_1 = []
+    coordinates_2 = []
+    until @computer_board.valid_placement?(cruiser, coordinates_1) do
+      coordinates_1 = coordinates_array.sample(cruiser.length)
+    end
+    @computer_board.place(cruiser,coordinates_1)
+    until @computer_board.valid_placement?(submarine, coordinates_2) do
+      coordinates_2 = coordinates_array.sample(submarine.length)
+    end
+    @computer_board.place(submarine,coordinates_2)
+  end
 
 
 end
