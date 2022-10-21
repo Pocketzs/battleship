db2376035183ed7a1631677c950caabd8a36dfac
Alastair Lloyd <dex8056@aol.com>
2022-10-21 13:39:49 -0600

    fix: Update game menu to only expect one input

--- lib/game.rb
+++ lib/game.rb
@@ -4,16 +4,31 @@ require './lib/board'
 
 class Game
 
-  def initialize
+  def initialize #may need may not......
   end
 
-  def player_input #thought it would be helpful to have a method to call on when user inputs rather than keep writing same code.
-    player1 = gets.chomp
+  def player1 #thought it would be helpful to have a method to call on when user inputs rather than keep writing same code.
+    player1 = gets.chomp.downcase
   end
 
   def game_menu
-    p "Welcome to BATTLESHIP"
-    p "Enter p to play. Enter q to quit"
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit"
+    input = player1
+
+    if input == "p"
+      #run game
+    elsif input == "q"
+      puts "Thanks for playing" #goodbye statement or similar
+    else
+      puts "Invalid please re enter choice" #or something similar
+    end
   end
+
+
+
+
+
+
 
 end
