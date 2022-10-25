41d6bba11c1c45f43fe9c7cfb9d6bfa511148107
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-25 12:21:37 -0500

    refactor: Delete game_board attr_reader

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,10 @@
 class Board
-  attr_reader :game_board
-
   def initialize
     @game_board
   end
 
   def cells
-    if game_board == nil
+    if @game_board == nil
       coordinates_array = ["A1","A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
       game_board = Hash.new
       coordinates_array.each do |coordinate|
